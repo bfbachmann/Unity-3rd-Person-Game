8f3da39fc7247dbf1a7f412440fb99ad4e0464a9
Bruno Bachmann <bfbachmann@gmail.com>
2016-03-26 19:39:05 -0700

    Added bird companion and modified trampoline motion to work in all cases.

---
+++ BirdController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdController : MonoBehaviour {
+
+	public GameObject player;
+	float maxDistance = 10;
+	Vector3 offset = new Vector3(0f, 4f, 0f);
+	float minSpeed = 6f;
+
+	void Start () {
+		gameObject.GetComponentInChildren<Animator> ().speed = 3;
+	}
+	
+	void Update () {
+
+		if ((player.transform.position - transform.position).magnitude <= minSpeed) {
+			circlePlayer ();
+		} else {
+			updateMovement ();
+		}
+		updateRotation ();	
+	}
+
+	void updateMovement () {
+		gameObject.GetComponent<Rigidbody>().velocity = player.transform.position + offset - transform.position;
+	}
+
+	void updateRotation () {
+		Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
+		transform.forward = rb.velocity;
+	}
+
+	void circlePlayer() {
+		float current = Time.time;
+//		transform.position = player.transform.position + new Vector3 (5* Mathf.Cos (current), 0f, 5*Mathf.Sin(current));
+		gameObject.GetComponent<Rigidbody>().velocity = player.transform.position - transform.position + offset + new Vector3 (5* Mathf.Cos (current), 0f, 5*Mathf.Sin(current));
+	}
+}
---
+++ Trampoline.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour {
+
+	bool moveUp = false;
+	Vector3 defaultPosition;
+
+	void Start () {
+		defaultPosition = transform.position;
+	}
+
+	void OnCollisionEnter () {
+		moveUp = true;
+	}
+
+	void OnCollisionExit () {
+		moveUp = false;
+	}
+
+	bool atMaxHeight() {
+		return (transform.position - defaultPosition).magnitude > 3f;
+	}
+
+	void Update () {
+		if (moveUp && !atMaxHeight()) {
+			gameObject.GetComponent<Rigidbody> ().velocity = transform.up * 20;
+		} else if (transform.position != defaultPosition) {
+			gameObject.GetComponent<Rigidbody> ().velocity = defaultPosition - transform.position;
+		}
+	}
+}
